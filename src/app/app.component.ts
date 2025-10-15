7da18851c046c12ac5b0c066dc49e02c19fab0e1
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2025-10-15 18:18:07 -0300

    Update app.component.ts
    Integração com API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service'; // ajuste o caminho se necessário
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `
+    <h1>Dados do Backend</h1>
+
+    <div *ngIf="carregando">Carregando...</div>
+
+    <div *ngIf="!carregando && dados">
+      <pre>{{ dados | json }}</pre>
+    </div>
+
+    <div *ngIf="!carregando && !dados">
+      Nenhum dado disponível.
+    </div>
+  `,
 })
-export class AppComponent {
-  value = 'World';
+export class AppComponent implements OnInit {
+  dados: any;
+  carregando = false;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.buscarDados();
+  }
+
+  buscarDados() {
+    this.carregando = true;
+    this.apiService.getData().subscribe({
+      next: (res) => {
+        this.dados = res;
+        this.carregando = false;
+      },
+      error: (err) => {
+        console.error('Erro ao chamar API:', err);
+        this.carregando = false;
+      },
+    });
+  }
 }
