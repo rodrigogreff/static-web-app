20ab1e0600a632a0cb72f5f6145b6e67bfc3764c
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2025-10-15 17:28:47 -0300

    Create api.service.ts
    API backend

---
+++ src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiUrl = 'https://backendapinet.azurewebsites.net'; // <- aqui é onde você define a URL do backend
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/endpoint`);
+  }
+}
