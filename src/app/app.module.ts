152911590074a4c875b4f6f9d0191040c4740312
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2025-10-15 17:32:35 -0300

    Update app.module.ts
    Comunicação com API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http"; // <- adicione isso
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule // <- e adicione aqui
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
